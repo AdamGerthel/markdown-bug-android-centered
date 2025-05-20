22e8f06c13d851b5c88608d3bfcc91b8732ab48d
Adam Gerthel <insats@gmail.com>
2025-05-20 12:12:30 +0200

    feat: add reproducible sample

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import Markdown, { MarkdownProps } from "react-native-marked";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const markdownStyles: MarkdownProps['styles'] = {
+  text: { textAlign: 'center' },
+  strong: { textAlign: 'center' }
+}
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+export default function App() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Markdown
+          styles={markdownStyles}
+          value={'**Hello** world'}
+        />
+        <Markdown
+          styles={markdownStyles}
+          value={'Hello **world**'}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+});
